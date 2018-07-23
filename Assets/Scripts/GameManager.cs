2877822ce49fb40f6a93d21930345f6d720ad5c1
JuneYeoungKim <sky14786@gmail.com>
2018-07-23 20:50:51 +0900

    프로젝트 생성

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new GameManager();
+            }
+            return instance;
+        }
+    }
+
+    public double Gold;
+}
---
+++ Assets/Scripts/UIMnager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMnager : MonoBehaviour {
+    private static UIMnager instance;
+    public static UIMnager Insatance
+    {
+        get
+        {
+            if(instance==null)
+            {
+                instance = new UIMnager();
+            }
+            return instance;
+        }
+    }
+
+    public Text GoldDisplay;
+
+    public void _Attack()
+    {
+        
+    }
+}
